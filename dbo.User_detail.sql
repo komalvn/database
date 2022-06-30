41db5f949ba795b4205fd0512687aab01f718817
komalvn <108405543+komalvn@users.noreply.github.com>
2022-06-30 10:08:35 +0530

    Add files via upload

---
+++ dbo.User_detail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User_detail] (
+    [Id]        VARCHAR (50) NOT NULL,
+    [FirstName] VARCHAR (50) NOT NULL,
+    [LastName]  VARCHAR (50) NOT NULL,
+    [Eamil]     VARCHAR (50) NOT NULL,
+    [contact]   BIGINT       NOT NULL,
+    [UserType]  INT          NOT NULL
+);
+
